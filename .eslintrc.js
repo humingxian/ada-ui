bdbeac1b614abc01bac0b6e220703a73fdd23796
humingxian <hudadacool@163.com>
2020-08-10 14:49:26 +0800

    init style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true,
+    es6: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'standard'
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 11,
-    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
   plugins: [
     'react',
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   rules: {
-  },
-};
+  }
+}
