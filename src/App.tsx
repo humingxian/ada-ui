c4dd92517620b256ec0ecbcd4d8a766154b8f39a
humingxian <hudadacool@163.com>
2020-08-11 09:47:02 +0800

    为 button 组件添加 原生属性;利用 交叉类型合并自定义props和原生属性props

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import Button from './components/Button/Button'
 
 function App () {
   return (
-    <div>
-      <main>
-        <Button btnType={'primary'}>normal-primary</Button>
-        <Button size={'sm'}>small</Button>
-        <Button btnType={'danger'} size={'lg'}>large-danger</Button>
-        <Button disabled={ true }>disabled</Button>
-        <Button btnType={'link'} href={'http://www.baidu.com'}>link-baidu.com</Button>
-        <Button btnType='link' href={'http://www.baidu.com'} disabled={true}>link-baidu.com</Button>
-      </main>
-    </div>
+    <main>
+      <Button btnType='primary' autoFocus={true}>normal-primary</Button>
+      <Button size='sm' onClick={e => { e.preventDefault(); alert(123) }}>small</Button>
+      <Button btnType='danger' size='lg'>large-danger</Button>
+      <Button disabled={ true }>disabled</Button>
+      <Button btnType='link' href='http://www.baidu.com' target='_blank'>link-baidu.com</Button>
+      <Button btnType='link' href='http://www.baidu.com' disabled={true}>link-baidu.com</Button>
+    </main>
   )
 }
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { AnchorHTMLAttributes } from 'react'
 import classNames from 'classnames'
 
 export type ButtonSize = 'lg' | 'sm'
@@ -16,27 +16,37 @@ interface BaseButtonProps {
   children?: React.ReactNode,
   href?: string
 }
-
-const Button: React.FC<BaseButtonProps> = (props) => {
+// 因为 Button 组件可以接受原生的 button 的属性和 a 元素的属性，所以必须a和button的原生属性都要支持
+// 联合类型 返回的是 a 或 b 的类型 用 ｜ 表示
+// 交叉类型 返回的是 a + b 的总类型 用 & 表示
+// React.ButtonHTMLAttributes 是 react 内部提供的原生button元素的接口
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+// Partial 是一个类型 他接受一个范型 最后生成的类型其内部的所有属性都是可选的。
+// （相当于在每个类型的后面添加了联合类型undefined）
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+const Button: React.FC<ButtonProps> = (props) => {
   const {
     btnType,
     disabled,
     size,
     children,
-    href
+    href,
+    className,
+    ...restProps
   } = props
-  const classes = classNames('btn', {
+  const classes = classNames('btn', className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     disabled: (btnType === 'link') && disabled
   })
   if (btnType === 'link' && href) {
     return (
-      <a href={href} className={classes}>{ children }</a>
+      <a href={href} className={classes} {...restProps}>{ children }</a>
     )
   } else {
     return (
-      <button className={classes} disabled={disabled}>{children}</button>
+      <button className={classes} disabled={disabled} {...restProps}>{children}</button>
     )
   }
 }
